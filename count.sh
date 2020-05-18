f98cb138b1b5d5eebe4e618072882c8ec30a0040
Samyak Bakliwal <w3bcode@gmail.com>
2020-05-18 18:34:44 +0530

    Edit count.sh to format in README file

--- count.sh
+++ count.sh
@@ -3,4 +3,4 @@
 til=$(fd -t f -e md -E README.md . './TIL' | wc -l)
 
 blog=$(fd -t f -e md -E README.md . './blog' | wc -l)
-echo "${blog} Blogs and ${til} TILs and counting..."
+echo "> _${blog} Blogs and ${til} TILs and counting..._"
